f62179daac32d6f33509e2c18c06583b51a1c17a
Rosen Yorgov <yorgov.h.r@gmail.com>
2025-05-30 14:40:43 +0300

    fixed lsp issue

---
+++ nvim/lua/plugins/lsp.lua
@@ -0,0 +1,105 @@
+return {
+{
+  "hrsh7th/nvim-cmp",
+
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",         -- LSP source for nvim-cmp
+    "hrsh7th/cmp-buffer",           -- Source for buffer words
+    "hrsh7th/cmp-path",             -- Source for filesystem paths
+    "L3MON4D3/LuaSnip",             -- Snippet engine
+    "saadparwaiz1/cmp_luasnip",     -- LuaSnip completion source
+    "rafamadriz/friendly-snippets", -- Predefined snippets
+  },
+
+  config = function()
+    local cmp = require("cmp")
+    local luasnip = require("luasnip")
+
+    require("luasnip.loaders.from_vscode").lazy_load()
+
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
+
+      window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      },
+
+      mapping = cmp.mapping.preset.insert({
+        -- The reason I unbound these keymaps is because
+        -- I can use the default <Ctrl n> <Ctrl p>, and also
+        -- because <Ctrl j> <Ctrl k> actually do stuff.
+        -- ["<C-k>"] = cmp.mapping.select_prev_item(),
+        -- ["<C-j>"] = cmp.mapping.select_next_item(),
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+      }),
+
+      sources = cmp.config.sources({
+        { name = "nvim_lsp", group_index = 2 },
+        { name = "luasnip",  group_index = 2 },
+        { name = "buffer",   group_index = 2 },
+        { name = "path",     group_index = 2 },
+      }),
+    })
+  end,
+},
+{
+  "neovim/nvim-lspconfig",
+
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+  },
+},
+{
+  "ray-x/lsp_signature.nvim",
+  event = "VeryLazy",
+  opts = {},
+  config = function(_, opts)
+    require("lsp_signature").setup(opts)
+  end,
+},
+{
+  "williamboman/mason.nvim",
+
+  dependencies = { "williamboman/mason-lspconfig.nvim" },
+
+  config = function()
+    require("mason").setup()
+    local mason_lspconfig = require("mason-lspconfig")
+
+    mason_lspconfig.setup({
+      automatic_installation = false,
+    })
+  end,
+},
+{
+  "nvimtools/none-ls.nvim",
+
+  config = function()
+    local null_ls = require("null-ls")
+
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.prettier,
+
+        -- null_ls.builtins.diagnostics.eslint_d.with({
+        --   condition = function(utils)
+        --     return utils.root_has_file(".eslintrc.js", ".eslintrc.json", ".eslintrc.cjs")
+        --   end,
+        -- }),
+      },
+    })
+  end,
+}
+
+}
