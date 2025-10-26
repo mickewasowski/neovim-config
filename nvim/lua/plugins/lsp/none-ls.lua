09d9a8a32165f16b6330d1d35b29f0ce54b53d09
Rosen Yorgov <yorgov.h.r@gmail.com>
2025-10-26 17:58:28 +0200

    additional configuration for eslint

--- nvim/lua/plugins/lsp/none-ls.lua
+++ nvim/lua/plugins/lsp/none-ls.lua
@@ -8,6 +8,9 @@ return {
       sources = {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.prettier,
+        null_ls.builtins.diagnostics.eslint_d,
+        null_ls.builtins.code_actions.eslint_d,
+        null_ls.builtins.formatting.eslint_d,
 
         -- null_ls.builtins.diagnostics.eslint_d.with({
         --   condition = function(utils)
